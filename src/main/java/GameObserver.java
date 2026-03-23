51f4a93dd70bf4c79596606b6590e3f6de698329
Alexander-Baker-1 <Alexander.Baker-1@colorado.edu>
2026-03-23 11:14:24 -0600

    Added GameObserver

--- src/main/java/GameObserver.java
+++ src/main/java/GameObserver.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public interface GameObserver {
+    void onCellRevealed(int row, int col);
+    void onGameWon();
+    void onGameLost();
+    void onFlagPlaced(int row, int col);
+    void onFlagRemoved(int row, int col);
 }
